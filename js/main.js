107cfbd645bf84acc3996dd3a0ab5c7231758da0
Quinton Jason <quinton.jason@gmail.com>
2025-02-25 12:51:33 -0600

    add console logs

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ menuToggle.addEventListener('click', () => {
   menuToggle.setAttribute('aria-expanded',!expanded);
   menu.classList.toggle('expanded');
 });
+
+
+const idElement = document.getElementById('top');
+console.log('idElement', idElement);
+
+const classElements = document.getElementsByClassName('purple-giraffe-container');
+console.log('classElements', classElements);
+
+const tagElements = document.getElementsByTagName('p');
+console.log('tagElement', tagElements);
+
+const QSelement = document.querySelector('.purple-giraffe-container');
+console.log('QSelement', QSelement);
+
+const QSAelement = document.querySelectorAll('.purple-giraffe-container');
+console.log('QSAelement', QSAelement);
+
+const beforGiraffes = document.querySelector('h1');
+console.log('beforGiraffes', beforGiraffes);
+// beforGiraffes.textContent = 'New content';
+beforGiraffes.innerHTML = '<p>New HTML content</p>';;
